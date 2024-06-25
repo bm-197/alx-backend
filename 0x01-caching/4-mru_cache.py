983156c6a4c61ee8aec938f80908348a34aad83e
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-06-25 15:06:04 +0300

    [Task-4] feat: Task 4 completed

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" LIFO caching module
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+class MRUCache(BaseCaching):
+    """ Represents an object that allows storing and
+    retrieving items from a dictionary with LIFO mechanis.
+    """
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+        self.cache_lines = OrderedDict()
+        self.line = 0
+    
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        
+        self.cache_lines[key] = self.line
+        self.line += 1
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+        if len(self.cache_data.items()) > BaseCaching.MAX_ITEMS:
+            k = sorted(self.cache_lines, key=lambda x: self.cache_lines[x])[-1]
+            self.cache_data.pop(k)
+            print("DISCARD: {}".format(k))
+        
+    def get(self, key):
+        """ Get an item in cache by key
+        """
+        if key is not None and key in self.catch_data:
+            self.catch_lines[key] = self.line
+            self.line += 1
+        return self.cache_data.get(key, None)
