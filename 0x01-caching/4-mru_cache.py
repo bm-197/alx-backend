72bc8d5af1bdf68261ac676819a30a33f857afdc
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-06-25 15:17:39 +0300

    [Task-4] update: Updated pop statement/n/nIt was removing the key we currently were inserting or getting, I modified it to discard the mru

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -25,14 +25,43 @@ class MRUCache(BaseCaching):
         self.cache_data[key] = item
         self.cache_data.move_to_end(key, last=True)
         if len(self.cache_data.items()) > BaseCaching.MAX_ITEMS:
-            k = sorted(self.cache_lines, key=lambda x: self.cache_lines[x])[-1]
+            k = sorted(self.cache_lines, key=lambda x: self.cache_lines[x])[-2]
             self.cache_data.pop(k)
+            self.cache_lines.pop(k)
             print("DISCARD: {}".format(k))
         
     def get(self, key):
         """ Get an item in cache by key
         """
-        if key is not None and key in self.catch_data:
-            self.catch_lines[key] = self.line
+        if key is not None and key in self.cache_data:
+            self.cache_lines[key] = self.line
             self.line += 1
         return self.cache_data.get(key, None)
+MRUCache = __import__('4-mru_cache').MRUCache
+
+my_cache = MRUCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+print(my_cache.get("B"))
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+print(my_cache.get("A"))
+print(my_cache.get("B"))
+print(my_cache.get("C"))
+my_cache.put("F", "Mission")
+my_cache.print_cache()
+my_cache.put("G", "San Francisco")
+my_cache.print_cache()
+my_cache.put("H", "H")
+my_cache.print_cache()
+my_cache.put("I", "I")
+my_cache.print_cache()
+my_cache.put("J", "J")
+my_cache.print_cache()
+my_cache.put("K", "K")
+my_cache.print_cache()
