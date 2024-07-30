d7c1a079eb839b936f9e10e50bf5ba7c3fad84c8
bm-197 <bisratmaru111@gmail.com>
2024-07-30 15:09:41 +0300

    [Task 2] feat: Completed Task 2

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,27 @@
+#!/usr/bin/yarn dev
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client not connected to server');
+});
+
+const setNewShool = (schoolName, value) => {
+  client.SET(schoolName, value, print);
+};
+
+const displaySchoolValue = (schoolName) => {
+  client.GET(schoolName, (_err, reply) => {
+    console.log(reply);
+  });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+
