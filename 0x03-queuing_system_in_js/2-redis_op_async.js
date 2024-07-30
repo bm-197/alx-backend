3501c403c611c244747ed3051920256726b70da6
bm-197 <bisratmaru111@gmail.com>
2024-07-30 16:35:01 +0300

    [Task 3] feat: Completed Task 3

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,32 @@
+#!/usr/bin/yarn dev
+import promisify from 'utils'
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to server:', err.toString());
+});
+
+const setNewShool = (schoolName, value) => {
+  client.SET(schoolName, value, print);
+};
+
+const get = promisfy(client.GET).bind(client);
+
+const displaySchoolValue = async (schoolName) => {
+  console.log(await get(schoolName));
+};
+
+async function main() {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+}
+
+
+client.on('connect', async () => {
+  console.log('Redis client not connected to server');
+  await main();
+});
+
